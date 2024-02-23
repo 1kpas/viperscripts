92cc67619ab2d23fb7ca7f8752c605df32aa0619
1kpas <98185858+1kpas@users.noreply.github.com>
2024-02-23 16:09:15 -0400

    Update bloquear-botao-direito.js

--- bloquear-botao-direito.js
+++ bloquear-botao-direito.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    setInterval(function() {
-        document.addEventListener('contextmenu', function(e) {
-            e.preventDefault();
-        });
-    }, 1000); // Verifica e reaplica o bloqueio a cada 1 segundo
+    document.addEventListener('contextmenu', function(e) {
+        e.preventDefault();
+    });
 });
