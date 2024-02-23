c0c491f2b457e165a8d85cd30578b707cb742b42
1kpas <98185858+1kpas@users.noreply.github.com>
2024-02-23 16:10:23 -0400

    Update bloquear-botao-direito.js

--- bloquear-botao-direito.js
+++ bloquear-botao-direito.js
@@ -1,11 +1,5 @@
-<script>
 document.addEventListener('DOMContentLoaded', function() {
-    // Impede o uso do botão direito do mouse na página
     document.addEventListener('contextmenu', function(e) {
         e.preventDefault();
-        return false;
     });
 });
-//Botão Direito
-//Fim
-</script>
