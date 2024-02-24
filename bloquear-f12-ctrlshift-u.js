81891206199e714956a5791ebd0ee905f2f7542e
1kpas <98185858+1kpas@users.noreply.github.com>
2024-02-24 05:03:52 -0400

    Update bloquear-f12-ctrlshift-u.js

--- bloquear-f12-ctrlshift-u.js
+++ bloquear-f12-ctrlshift-u.js
@@ -1,21 +1,19 @@
-//Começo
-//Bloqueador de F12 + Ctrl Shift U
-// Função para bloquear combinações de teclas específicas
-function bloquearAtalhos(evento) {
-    // Bloquear Ctrl+Shift+U
-    if (evento.ctrlKey && evento.shiftKey && evento.keyCode == 85) {
-        console.log('Ctrl+Shift+U bloqueado.');
-        evento.preventDefault();
+(function() {
+    // Função para bloquear combinações de teclas específicas
+    function bloquearAtalhos(evento) {
+        // Bloquear Ctrl+Shift+I (ou U dependendo do navegador)
+        if (evento.ctrlKey && evento.shiftKey && (evento.keyCode == 73 || evento.keyCode == 85)) {
+            console.log('Ctrl+Shift+I/U bloqueado.');
+            evento.preventDefault();
+        }
+
+        // Bloquear F12
+        if (evento.keyCode == 123) {
+            console.log('F12 bloqueado.');
+            evento.preventDefault();
+        }
     }
 
-    // Bloquear F12
-    if (evento.keyCode == 123) {
-        console.log('F12 bloqueado.');
-        evento.preventDefault();
-    }
-}
-
-// Adicionar listener para o evento de pressionamento de tecla
-document.addEventListener('keydown', bloquearAtalhos);
-//Bloqueador de F12 + Ctrl Shift U
-//fim
+    // Adicionar listener para o evento de pressionamento de tecla
+    document.addEventListener('keydown', bloquearAtalhos);
+})();
