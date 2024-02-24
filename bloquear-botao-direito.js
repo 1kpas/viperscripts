fbf6da56a2e74c28cdeb8b62dd9a18e278cce7c4
1kpas <98185858+1kpas@users.noreply.github.com>
2024-02-24 04:53:46 -0400

    Update bloquear-botao-direito.js

--- bloquear-botao-direito.js
+++ bloquear-botao-direito.js
@@ -1,13 +1,6 @@
-<script>
-document.addEventListener('DOMContentLoaded', () => {
-    // Desabilita a seleção de texto
-    document.addEventListener('mousedown', (e) => {
-        e.preventDefault();
-    }, false);
-
-    // Desabilita o menu de contexto do clique com o botão direito
-    document.addEventListener('contextmenu', (e) => {
-        e.preventDefault();
-    }, false);
-});
-</script>
+document.oncontextmenu = function() {
+    return false;
+}
+document.onselectstart = function() {
+    return false;
+}
