84d290bca0320d5b6d57fa360937a30a42e5d65c
1kpas <98185858+1kpas@users.noreply.github.com>
2024-02-24 04:19:52 -0400

    Update bloquear-botao-direito.js

--- bloquear-botao-direito.js
+++ bloquear-botao-direito.js
@@ -1,19 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Bloqueia copiar
-    document.addEventListener('copy', (e) => {
+    // Desabilita a seleção de texto
+    document.addEventListener('mousedown', (e) => {
         e.preventDefault();
-        alert('Copiar conteúdo não é permitido.');
-    });
+    }, false);
 
-    // Bloqueia recortar
-    document.addEventListener('cut', (e) => {
-        e.preventDefault();
-        alert('Recortar conteúdo não é permitido.');
-    });
-
-    // Bloqueia o menu de contexto para impedir a cópia pelo botão direito
+    // Desabilita o menu de contexto do clique com o botão direito
     document.addEventListener('contextmenu', (e) => {
         e.preventDefault();
-        alert('O uso do botão direito é restrito nesta página.');
-    });
+    }, false);
 });
